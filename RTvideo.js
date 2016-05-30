916b0c069babcfd53905242c68a2192ad2697045
flyrainning <flyrainning@gmail.com>
2016-05-31 07:39:24 +0800

    Update RTvideo.js
    精简post的参数，增加stylewidth和styleheight配置

--- RTvideo.js
+++ RTvideo.js
@@ -20,15 +20,21 @@ video.stop();
 function RTvideo(opt){
 	this.opt=$.extend({//这里写默认配置
 		server:"ws://172.0.8.5:8080",
+		key:"",
+		id:"",
+		
 		width:320,
 		height:240,
 		format:'jpg',
+		
+		stylewidth:"320px",
+		styleheight:"240px",
+		
 		FPS:10,
-		key:"",
-		id:"",
-		bindtype:"",
+
 		server_side_encode:false,
 		opt:75,
+		
 		autostart:false,
 		
 	
@@ -54,6 +60,18 @@ function RTvideo(opt){
 	this.FPS();
 	if (this.opt.id) this.bind(this.opt.id);
 	
+	this.sendopt={
+		act:"RTvideo_get",
+		key:this.opt.key	
+	}
+	if (this.opt.server_side_encode){
+		this.sendopt.width=this.opt.width;
+		this.sendopt.height=this.opt.height;
+		this.sendopt.format=this.opt.format;
+		this.sendopt.server_side_encode=this.opt.server_side_encode;
+		this.sendopt.opt=this.opt.opt;
+	}
+	
 	if (this.opt.autostart){
 		this.ws.open(function(){that.start();});
 		
@@ -68,8 +86,8 @@ RTvideo.prototype.bind=function(id){
 	this.opt.bindtype=obj.tagName;
 	if (this.opt.bindtype=="IMG"){
 		this.image =obj;
-		this.image.style.width = this.opt.width+"px";
-		this.image.style.height = this.opt.height+"px";
+		this.image.style.width = this.opt.stylewidth;
+		this.image.style.height = this.opt.styleheight;
 		this.image.onload = function(e) {
 				that.revokeObjectURL(that.image.src); // 清除释放
 			};
@@ -84,8 +102,8 @@ RTvideo.prototype.bind=function(id){
 			};
 		this.canvas=obj;
 		this.ctx=this.canvas.getContext('2d');
-		this.canvas.style.width = this.opt.width+"px";
-		this.canvas.style.height = this.opt.height+"px";
+		this.canvas.style.width = this.opt.stylewidth;
+		this.canvas.style.height = this.opt.styleheight;
 	}else{
 		console.log("We need <img> or <canvas>");
 	}
@@ -95,15 +113,15 @@ RTvideo.prototype.start=function(){
 	if (!this.isstart){
 		var that=this;
 		this.isstart=true;
-		this.timer=setInterval(function(){that.check();},1000);
+		this.timer=setInterval(function(){that.check();},2000);
 		this.getframe();
 	}
 	
 }
 RTvideo.prototype.error=function(evt){
-	console.log('error');
-	console.log(this);
-	console.log(evt);
+	//console.log('error');
+	//console.log(this);
+	//console.log(evt);
 }
 RTvideo.prototype.stop=function(){
 	this.isstart=false;
@@ -118,28 +136,20 @@ RTvideo.prototype.FPS=function(fps){
 RTvideo.prototype.getframe=function(){
 	if (!this.isstart) return;
 	this.lasttime=new Date().getTime();
-	this.ws.send({
-		act:"RTvideo_get",
-		key:this.opt.key,
-		width:this.opt.width,
-		height:this.opt.height,
-		format:this.opt.format,
-		server_side_encode:this.opt.server_side_encode,
-		opt:this.opt.opt
 	
-	});
+	this.ws.send(this.sendopt);
 
 }
 RTvideo.prototype.check=function(){
 
 	var now=new Date().getTime();
 	var passtime=now-this.lasttime;
-	if ((now-this.lasttime)>1000) this.getframe();
+	if ((now-this.lasttime)>3500) this.getframe();
 
 }
 RTvideo.prototype.draw=function(data,evt){
 	var that=this;
-console.log('d');
+
 	var now=new Date().getTime();
 	var passtime=now-that.lasttime;
 	var runtime=that.timebtw-(now-that.lasttime);
